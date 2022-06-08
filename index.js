a65a8710abe5007936ed52ac1c471d091aedd8df
David Bilson <85068757+dlbi@users.noreply.github.com>
2022-06-08 14:12:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function loadmsg() {
+    alert("Hi. This is an NFT prototype website. It is not a full-fledged website. Kindly Navigate through.");
+};
+function clickFeedback() {
+    alert("Hello! This is not a live minting website. Kindly Navigate through.");
+};
